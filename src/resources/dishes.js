8d54c2de8557d78e7c267d51f6c8a2ac034fca98
Audun Skjervold <audun@skjervold.me>
2017-08-17 16:09:15 +0200

    Add example list of "Norwegian" dishes

---
+++ src/resources/dishes.js
@@ -0,0 +1,52 @@
+const dishes = [
+    {
+        "name": "Taco",
+        "protein": "kjøttdeig",
+        "category": "texmex",
+        "time": 2
+    },
+    {
+        "name": "Enchiladas",
+        "protein": "kylling",
+        "category": "texmex",
+        "time": 3
+    },
+    {
+        "name": "Crispy chicken",
+        "protein": "kylling",
+        "category": "texmex",
+        "time": 2
+    },
+    {
+        "name": "Pizza",
+        "protein": "kjøttdeig",
+        "category": "helg",
+        "time": 3
+    },
+    {
+        "name": "Tikka",
+        "protein": "kylling",
+        "category": "asiatisk",
+        "time": 2
+    },
+    {
+        "name": "Peanøttkylling",
+        "protein": "kylling",
+        "category": "asiatisk",
+        "time": 3
+    },
+    {
+        "name": "Sodd",
+        "protein": "none",
+        "category": "suppe",
+        "time": 3
+    },
+    {
+        "name": "Tomatsuppe",
+        "protein": "none",
+        "category": "suppe",
+        "time": 2
+    },
+];
+
+export default dishes;
