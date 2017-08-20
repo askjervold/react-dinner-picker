5ee8241113677f8c82c6bbe9865a99eea4be17ec
Audun Skjervold <audun@skjervold.me>
2017-08-21 00:51:37 +0200

    Add more dishes

--- src/resources/dishes.js
+++ src/resources/dishes.js
@@ -37,15 +37,201 @@ const dishes = [
     },
     {
         "name": "Sodd",
-        "protein": "none",
+        "protein": "none/self",
         "category": "suppe",
         "time": 3
     },
     {
         "name": "Tomatsuppe",
-        "protein": "none",
+        "protein": "none/self",
+        "category": "suppe",
+        "time": 1
+    },
+    {
+        "name": "Flatburger",
+        "protein": "none/self",
+        "category": "junk",
+        "time": 1
+    },
+    {
+        "name": "Hjemmelaget burger",
+        "protein": "kjøttdeig",
+        "category": "junk",
+        "time": 2
+    },
+    {
+        "name": "Pestopizza",
+        "protein": "kylling",
+        "category": "italiensk",
+        "time": 3
+    },
+    {
+        "name": "Kjøttboller",
+        "protein": "none/self",
+        "category": "italiensk",
+        "time": 1
+    },
+    {
+        "name": "Hønsefrikassé",
+        "protein": "none/self",
+        "category": "gryter",
+        "time": 4
+    },
+    {
+        "name": "Quesedillas",
+        "protein": "kylling",
+        "category": "texmex",
+        "time": 3
+    },
+    {
+        "name": "Blomkålsuppe",
+        "protein": "none/self",
         "category": "suppe",
         "time": 2
+    },
+    {
+        "name": "Koteletter",
+        "protein": "none/self",
+        "category": "husmannskost",
+        "time": 2
+    },
+    {
+        "name": "Kjøttkaker",
+        "protein": "none/self",
+        "category": "husmannskost",
+        "time": 2
+    },
+    {
+        "name": "Lapskaus",
+        "protein": "biff",
+        "category": "husmannskost",
+        "time": 4
+    },
+    {
+        "name": "Biff",
+        "protein": "biff",
+        "category": "helg",
+        "time": 3
+    },
+    {
+        "name": "Stroganoff",
+        "protein": "biff",
+        "category": "gryter",
+        "time": 3
+    },
+    {
+        "name": "Kebab",
+        "protein": "løvbiff",
+        "category": "junk",
+        "time": 3
+    },
+    {
+        "name": "Kebabkjøttdeig",
+        "protein": "kjøttdeig",
+        "category": "junk",
+        "time": 2
+    },
+    {
+        "name": "Kyllingwok",
+        "protein": "kylling",
+        "category": "asiatisk",
+        "time": 2
+    },
+    {
+        "name": "Biffwok",
+        "protein": "løvbiff",
+        "category": "asiatisk",
+        "time": 2
+    },
+    {
+        "name": "Kylling chop suey",
+        "protein": "kylling",
+        "category": "asiatisk",
+        "time": 2
+    },
+    {
+        "name": "Biff chop suey",
+        "protein": "løvbiff",
+        "category": "asiatisk",
+        "time": 2
+    },
+    {
+        "name": "Meksikansk gryte",
+        "protein": "kjøttdeig",
+        "category": "gryter",
+        "time": 2
+    },
+    {
+        "name": "Kalkun",
+        "protein": "none/self",
+        "category": "helg",
+        "time": 4
+    },
+    {
+        "name": "Stek (lam/okse/svin)",
+        "protein": "none/self",
+        "category": "helg",
+        "time": 4
+    },
+    {
+        "name": "Lasagne",
+        "protein": "kjøttdeig",
+        "category": "italiensk",
+        "time": 3
+    },
+    {
+        "name": "Kylling og ris",
+        "protein": "kylling",
+        "category": "diverse",
+        "time": 2
+    },
+    {
+        "name": "Vårruller",
+        "protein": "kjøttdeig",
+        "category": "asiatisk",
+        "time": 4
+    },
+    {
+        "name": "Andebryst",
+        "protein": "none/self",
+        "category": "helg",
+        "time": 3
+    },
+    {
+        "name": "Nachos",
+        "protein": "kjøttdeig",
+        "category": "texmex",
+        "time": 3
+    },
+    {
+        "name": "Pastaform",
+        "protein": "kylling",
+        "category": "diverse",
+        "time": 2
+    },
+    {
+        "name": "Bolognese",
+        "protein": "kjøttdeig",
+        "category": "italiensk",
+        "time": 2
+    },
+    {
+        "name": "Kyllingsalat",
+        "protein": "kylling", // grillet eller marinert
+        "category": "diverse",
+        "time": 2
+    },
+    {
+        "name": "Pannekaker",
+        "protein": "bacon",
+        "category": "husmannskost",
+        "time": 3
+    },
+    {
+        "name": "BBQ Melt",
+        "protein": "kylling",
+        "category": "helg",
+        "time": 3
     },
 ];
 
